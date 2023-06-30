88794eae5a117cc4b7fd292a671c0c359deceba4
Cássio Luis <ckr.luis@gmail.com>
2023-06-30 10:55:50 -0300

    Subindo projeto API com Cypress

---
+++ cypress/support/commands/services.js
@@ -0,0 +1,71 @@
+
+Cypress.Commands.add('postUser', (user) => {
+    cy.api({
+        url: '/users',
+        method: 'POST',
+        body: user,
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
+
+Cypress.Commands.add('postSession', (user) => {
+    const payload =
+    {
+        email: user.email,
+        password: user.password
+    }
+
+    cy.api({
+        url: '/sessions',
+        method: 'POST',
+        body: payload,
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
+
+
+Cypress.Commands.add('postTask', (task, token) => {
+    cy.api({
+        url: '/tasks',
+        method: 'POST',
+        body: task,
+        headers: { authorization: token },
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
+
+Cypress.Commands.add('getTasks', (token) => {
+    cy.api({
+        url: '/tasks',
+        method: 'GET',
+        headers: { authorization: token },
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
+
+Cypress.Commands.add('getUniqueTask', (taskId, token) => {
+    cy.api({
+        url: '/tasks/' + taskId,
+        method: 'GET',
+        headers: { authorization: token },
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
+
+Cypress.Commands.add('deleteTask', (taskId, token) => {
+    cy.api({
+        url: '/tasks/' + taskId,
+        method: 'DELETE',
+        headers: { authorization: token },
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
+
+Cypress.Commands.add('putTaskDone', (taskId, token) => {
+    cy.api({
+        url: `/tasks/${taskId}/done`,
+        method: 'PUT',
+        headers: { authorization: token },
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
